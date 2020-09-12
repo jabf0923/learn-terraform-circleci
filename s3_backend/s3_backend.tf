cf0d1a6161598f497f4f7054d8c1684eb2394015
jabf0923 <jabf0923@gmail.com>
2020-09-12 13:43:42 -0500

    new push

--- s3_backend/s3_backend.tf
+++ s3_backend/s3_backend.tf
@@ -9,14 +9,14 @@ terraform {
 resource "random_uuid" "randomid" {}
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "${random_uuid.randomid.result}-backend"
+bucket = "${random_uuid.randomid.result}-backend"
   # Enable versioning so we can see the full revision history of our
   # state files
   force_destroy = true
   versioning {
     enabled = true
   }
-  # Enable server-side encryption by default
+# Enable server-side encryption by default
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
