b136a4e3ff63a17a4355fcf14be16733e9c8f6c7
jabf0923 <jabf0923@gmail.com>
2020-09-12 22:28:08 -0500

    remove double autoscaling resource

--- main.tf
+++ main.tf
@@ -186,14 +186,3 @@ resource "aws_launch_template" "foobar" {
   instance_type = "t2.micro"
 }
 
-resource "aws_autoscaling_group" "bar1" {
-  availability_zones = ["us-east-1a"]
-  desired_capacity   = 1
-  max_size           = 2
-  min_size           = 1
-
-  launch_template {
-    id      = aws_launch_template.foobar.id
-    version = "$Latest"
-  }
-}
