ec8f4f5e32c216ec427e90277e44e10d65456cd8
jabf0923 <jabf0923@gmail.com>
2020-09-12 21:19:20 -0500

    adding a vpc

--- main.tf
+++ main.tf
@@ -75,3 +75,11 @@ resource "aws_s3_bucket_object" "app" {
 output "Endpoint" {
   value = aws_s3_bucket.app.website_endpoint
 }
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
